b380b56e7c7d71c33e6861cbb1b603fa281699a9
bhatia4404 <shivambhatia4404@gmail.com>
2024-02-07 21:39:35 +0530

    Initial Commit

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(express.json());
-app.use(
-  cors({
-    origin: "http://127.0.0.1:8080",
-  })
-);
+app.use(cors());
 
 const userRouter = require("./routes/user.js");
 app.use("/user", userRouter);
