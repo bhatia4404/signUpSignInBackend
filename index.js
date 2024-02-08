54358965a48a0183dbd9b36ccb5b14d68e657357
bhatia4404 <shivambhatia4404@gmail.com>
2024-02-08 11:37:59 +0530

    Added origin localhost

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://65c3b32f56e21a501976c8a3--jazzy-bienenstitch-10eb99.netlify.app",
+
+      "http://127.0.0.1:8080",
+    ],
+  })
+);
 
 const userRouter = require("./routes/user.js");
 app.use("/user", userRouter);
